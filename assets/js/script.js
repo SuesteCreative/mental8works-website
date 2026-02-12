83ef62b08efadb82cf0429f057b87ee0db439b15
Sueste - Creative Agency <info@sueste-creative.pt>
2026-02-12 12:26:41 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-// Luxury Preloader Handler
-window.addEventListener('load', function () {
-    const preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.classList.add('fade-out');
-        setTimeout(() => {
-            preloader.style.display = 'none';
-        }, 800);
-    }
-});
-
 // Global Functions for HTML interactions
 window.openModal = function (id) {
     const modal = document.getElementById(id);
